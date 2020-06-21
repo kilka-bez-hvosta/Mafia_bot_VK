7382d59b2829b94e671dedcfaa48eae84f13ad8c
kilka-bez-hvosta <akopyannairaa@yandex.ru>
2020-06-21 17:45:20 +0300

    distribution of roles

--- head file.py
+++ head file.py
@@ -3,8 +3,8 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 
 
-class Game:
-    pass
+GAME_VARIATIONS = [['мафия' * 2, "доктор", "комиссар", "ночная бабочка", "маньяк"],
+                                       ["мафия" * 2, "комиссар", "доктор"], ["маньяк", "комиссар"]]
 
 
 def main():
@@ -17,8 +17,28 @@ def main():
     for event in longpoll.listen():
         print(event)
         if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat:
-            if event.text == 'Начали!':
-                pass
+            if event.obj.message['text'] == 'Кто играет - ставьте плюс:' and event.chat_id not in games.keys() :
+                games[event.chat_id] = ['подключение игроков', dict()]
+            if event.chat_id in games.keys() and games[event.chat_id][0] == 'подключение игроков':
+                if event.obj.message['text'] == '+':
+                    games[event.chat_id][1][event.from_id] = 'мирный житель'
+                if event.obj.message['text'] == 'Начали!':
+
+                    games[event.chat_id][0] = 'идет игра'
+                    for player in games[event.chat_id][1].keys():
+                        if ((len(games[event.chat_id][1].keys()) == 5)):
+                            variant = GAME_VARIATIONS[-1]
+                        elif ((len(games[event.chat_id][1].keys()) <= 8)):
+                            variant = GAME_VARIATIONS[-2]
+                        elif ((len(games[event.chat_id][1].keys()) >= 8)):
+                            variant = GAME_VARIATIONS[0]
+                        variant[-1] += ["мирный житель"]
+                        games[event.chat_id][1][player] = variant[-1].pop(variant[-1].index(random.choice(variant[-1])))
+
+                        vk.messages.send(user_id=event.obj.message['from_id'],
+                                        message=games[event.chat_id][1][player],
+                                        random_id=random.randint(0, 2 ** 64))
+
 
 
 
